859669a869ced9f5bdf4a4b9bf813adaa0db89f4
aishwaryashelke16geobull <aishwaryashelke16.geobull@gmail.com>
2026-06-13 11:44:56 +0530

    My local changes

---
+++ lib/features/DashBoard/View/dashboard_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+import '../Controller/dashboard_controller.dart';
+
+class DashboardView extends StatelessWidget{
+
+  final DashboardController controller = Get.put(DashboardController());
+  @override
+  Widget build(BuildContext context) {
+
+  }
+
+}
